6bcf902baba18a792415b9ed6fe25d299905ce45
hyesung99 <4538asd@naver.com>
2023-04-28 03:12:06 +0900

    add getMainContet

--- client/src/routes/main.component.jsx
+++ client/src/routes/main.component.jsx
@@ -3,14 +3,33 @@ import './main.style.scss';
 import Map from '../components/map/map.component';
 import { UserContext } from '../contexts/user.context';
 import { ProjectContext } from '../contexts/project.context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import axios from "axios";
+
+const defaultMainContent = {
+  chattingLog : '',
+  places : '',
+  member : '',
+}
 
 const MainPage = () => {
 
   const {currentProject} = useContext(ProjectContext);
   const {cureentUser} = useContext(UserContext);
+  const [mainContent,setMainContent] = useState(defaultMainContent);
 
-  console.log(currentProject);
+  useEffect(() =>{
+
+    async function getMainContent(){
+      try{
+        const response = await axios.get('/mainContent');
+        setMainContent(response.data);
+      } catch (error){
+        console.log(error);
+      }
+    }
+    getMainContent();
+  },[mainContent]);
 
   return(
     <div className='main-page-wrapper'>
--- client/src/routes/project-list.component.jsx
+++ client/src/routes/project-list.component.jsx
@@ -1,22 +1,33 @@
 import ProjectForm from '../components/project-form/project-form.component';
 import { useContext } from 'react';
 import { ProjectContext } from '../contexts/project.context';
-import './project-list-style.scss';
+import './project-list.style.scss';
+
+
 
 const ProjectList = () => {
 
   const { projectList } = useContext(ProjectContext);
-  console.log(projectList);
+  
   return (
-    <div className='project-wrapper'>
-      {projectList.map((project) => (
-        <ProjectForm key={project.id} project={project}/>
-      ))}
-      <button onClick={()=>{}}>
-        +프로젝트 추가
+    <div className='project-body'>
+      <button className='prev-btn'>
+         {'<'}
+      </button>
+      <div className='project-slider'>
+        <button onClick={()=>{}}>
+          +프로젝트 추가
+        </button>
+        {projectList.map((project) => (
+          <ProjectForm key={project.id} project={project}/>
+        ))}
+      </div>
+      <button className='next-btn'>
+         {'>'}
       </button>
     </div>
   );
 };
 
 export default ProjectList;
+
