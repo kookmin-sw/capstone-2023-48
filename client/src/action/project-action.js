76b8c89e9ad1e430616a4cd6ad833ec3de88cad2
kimkh1228 <kimkhkimhw@kookmin.ac.kr>
2023-05-06 16:18:22 +0900

    add getProjectList

---
+++ client/src/action/project-action.js
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from 'axios';
+
+axios.defaults.withCredentials = true; // withCredentials 전역 설정
+
+export const api = axios.create({
+    baseURL: 'http://localhost:5001/api',
+});
+
+export async function getProjectList(userId) {
+    const response = await api.get(`/projects/${userId}`);
+    return response;
+}
