26791b2df37d8735543e181f50b040a9fe2b9af6
hyesung99 <4538asd@naver.com>
2023-05-09 17:50:12 +0900

    fix result img list

--- client/src/components/sideBar/map/searchResult.component.jsx
+++ client/src/components/sideBar/map/searchResult.component.jsx
@@ -1,16 +1,31 @@
 import './searchResult.style.scss';
 import { addPlace } from '../../../action/plan-action';
 import { ProjectContext } from '../../../contexts/project.context';
-import { useContext } from 'react';
+import { useContext, useState, useRef, useEffect } from 'react';
 
 const SearchResult = (props) =>{
-  const {result} = props;
-  console.log(result);
-
+  
+  const { result } = props;
   const { setCurrentProject } = useContext(ProjectContext);
+  const [ toggleImgList, setToggleImgList ] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideRef = useRef(null);
+  const TOTAL_SLIDES = result.photos.length-1;
+
+  useEffect(() => {
+    if(slideRef.current){
+      slideRef.current.style.transition = 'all 0.5s ease-in-out';
+      slideRef.current.style.transform = `translateX(calc(-${currentSlide} * 600px))`; // 백틱을 사용하여 슬라이드로 이동하는 에니메이션을 만듭니다.
+    }
+  },[currentSlide])
+
+  //toggle img list isVisible when img clicked
+  const handleImgClick = () => {
+    setToggleImgList(!toggleImgList);
+  }
 
   //add place to plan
-  const handleClick = async () => {
+  const handleAddBtnClick = async () => {
     await addPlace(
       result.name,
       result.formatted_address,
@@ -22,14 +37,55 @@ const SearchResult = (props) =>{
     })
   }
 
+  const handlePrevBtnClick = () => {
+    if (currentSlide === 0) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }  
+  }
+  
+  const handleNextBtnClick = () => {
+    if (currentSlide === TOTAL_SLIDES) {
+      setCurrentSlide(TOTAL_SLIDES);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
+  }
+
   return(
     <div className='result-wrapper'>
+      {toggleImgList && 
+        // <ResultImgList key={result.photos.id} photos={result.photos} ref={slideRef}/>
+        <div className='result-img-list' ref={slideRef}>
+          {result.photos.map((photo) => {
+            return(
+              <div className='result-img-list-item' key={photo.id}style={{ backgroundImage: `url(${photo.getUrl()})`}}/>
+            )
+          })}
+        </div>
+      }
+      {toggleImgList &&
+        <button 
+          className='result-img-list-prev-btn'
+          onClick={handlePrevBtnClick}
+          >&lt;</button> 
+        }
+      {toggleImgList && 
+        <button className='result-img-list-next-btn'
+          onClick={handleNextBtnClick}
+        >&gt;
+        </button>  
+      }
       <div className="result-container">
         <div className='result-img-wrapper'>
-          { result.photos && <div className='result-img' style={{ 
-            backgroundImage: `url(${result.photos[0].getUrl()})` 
-            }}>
-            </div>
+          { result.photos && 
+          <div className='result-img' 
+            style={{ backgroundImage: `url(${result.photos[0].getUrl()})`}}
+            onClick={handleImgClick}
+          >
+          +{result.photos.length}
+          </div>
           }
           { !result.photos && <div className='result-img'></div> }
           
@@ -40,7 +96,7 @@ const SearchResult = (props) =>{
           <div className='result-rating'>{result.rating} {result.user_ratings_total}</div>
         </div>
         <div className='result-add-btn-wrapper'>
-          <button className='result-add-btn' onClick={handleClick}>추가</button>
+          <button className='result-add-btn' onClick={handleAddBtnClick}>추가</button>
         </div>
       </div>
     </div>
