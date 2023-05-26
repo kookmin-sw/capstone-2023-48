fa3f8c0ea1df380b4d09f2a6892f16691742566b
kimkh1228 <kimkhkimhw@kookmin.ac.kr>
2023-05-26 15:00:27 +0900

    fix detail

--- client/src/action/project-action.js
+++ client/src/action/project-action.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = true; // withCredentials 전역 설정
 
 export const api = axios.create({
-    baseURL: 'http://35.78.64.250/:5001/api',
+    baseURL: 'http://35.78.64.250:5001/api',
 });
 
 export async function getProjectList(userId) {
